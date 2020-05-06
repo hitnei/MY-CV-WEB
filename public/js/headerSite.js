cebbf4d47cb6bea0b9143cf37aacd51f13aea4eb
hitnei <hoangnhi050298@gmail.com>
2020-05-06 17:30:41 +0700

    headerSite

---
+++ public/js/headerSite.js
@@ -0,0 +1,9 @@
+// function hoverBtnDownload() {
+//     var btnDownloadCV = document.getElementById('btnDownloadCV')
+//     btnDownloadCV.addEventListener('mouseenter', function (e) {
+//         btnDownloadCV.style.color = '#F0F'
+//     })
+
+// }
+
+// hoverBtnDownload()
