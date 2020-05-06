d0a7dfee4e5b2da9c241922e977e61fb61a0537f
hitnei <hoangnhi050298@gmail.com>
2020-05-06 16:22:12 +0700

    fix bg active all page

--- public/js/bgAnimation.js
+++ public/js/bgAnimation.js
@@ -1,6 +1,7 @@
 function bgAni() {
+    var body = document.body
     var bgAni = document.getElementById('bgAni')
-    bgAni.addEventListener('mousemove', function (event) {
+    body.addEventListener('mousemove', function (event) {
         var moveX = (event.pageX * -1 / 30)
         var moveY = (event.pageY * -1 / 30)
         bgAni.style.backgroundPosition = moveX + 'px ' + moveY + 'px'
