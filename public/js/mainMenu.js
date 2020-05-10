bd66f2115ecff067a9ba4db2cc01c2089823e3a5
hitnei <hoangnhi050298@gmail.com>
2020-05-10 13:30:27 +0700

    change site

--- public/js/mainMenu.js
+++ public/js/mainMenu.js
@@ -1,13 +1,31 @@
 var listLi = document.querySelectorAll(".mainMenu li")
 var nameIconSelected = "home"
 
+var currentHash = 'homeSite'
+var currentSite = document.getElementById(currentHash)
+currentSite.classList.add('showSite')
+window.location.hash = 'homeSite'
+
 listLi.forEach(function (li) {
     li.addEventListener('click', function () {
         // remove class active in current icon selected
         document.querySelector(`.mainMenu #${nameIconSelected} svg`).classList.remove('active')
+
         // add new class active
         nameIconSelected = li.getAttribute('id')
         changeIconSelected()
+
+        // setting
+        currentHash = window.location.hash.slice(1)
+        currentHash = currentHash === 'homeSite' ? 'homeSite' : currentHash + 'Site'
+        currentSite = document.getElementById(currentHash)
+
+        // remove class showSite
+        currentSite.classList.remove('showSite')
+
+        // add class showSite
+        var nowSite = document.getElementById(nameIconSelected + 'Site')
+        nowSite.classList.add('showSite')
     })
 })
 
